1aa817b970835ce32e09185c5ef73b74ae704198
윤주영 <pulpul8282@naver.com>
2021-05-15 21:54:29 +0900

    [첫 테스트 코드]

---
+++ src/test/java/org/yjy/yjy/YjyApplicationTests.java
@@ -0,0 +1,14 @@
+package org.yjy.yjy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YjyApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println(" contextLoads");
+    }
+
+}

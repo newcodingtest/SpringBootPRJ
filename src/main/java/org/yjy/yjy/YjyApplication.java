40cb8e383b8685f548855ec6b257bb665d9e0aaf
윤주영 <pulpul8282@naver.com>
2021-05-15 21:53:40 +0900

    [스프링 부트의 시작점 위치]

---
+++ src/main/java/org/yjy/yjy/YjyApplication.java
@@ -0,0 +1,13 @@
+package org.yjy.yjy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YjyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YjyApplication.class, args);
+    }
+
+}
